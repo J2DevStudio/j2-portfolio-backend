d58e7311cf8bea8fe6e5273a2750a7fbf8d97ea4
J2DevStudio <j2devstudio@gmail.com>
2023-05-27 13:27:37 -0400

    ff structure created - schema created

--- sanity.config.js
+++ sanity.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+// schemas are defined in the './schemas' folder
 
 export default defineConfig({
   name: 'default',
